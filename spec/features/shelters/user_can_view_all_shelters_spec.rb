c010ac8c179264fc2eaf97740e87264a3121c7c9
Fred Rondina <fredrondina96@gmail.com>
2020-03-21 19:45:17 -0600

    Completed user story 17 plus tests

--- spec/features/shelters/user_can_view_all_shelters_spec.rb
+++ spec/features/shelters/user_can_view_all_shelters_spec.rb
@@ -14,4 +14,16 @@ RSpec.describe "shelters index page", type: :feature do
     expect(page).to have_content(shelter3.name)
 
   end
+
+  it "all shelter names are links to the shelter show page" do
+    shelter1 = Shelter.create(name: 'Ricardos Reptiles', address: "1234 st", city: 'Denver', state: 'Colorado', zip: "29572")
+    shelter2 = Shelter.create(name: "Freds Felines", address: "5678 st", city: 'Seattle', state: 'Washington', zip: "390507")
+    shelter3 = Shelter.create(name: "Monroes Monitors", address: '2948 st', city: 'San Diego', state: 'California', zip: "30958")
+
+    visit "/shelters"
+
+    click_link("#{shelter1.name}")
+
+    expect(current_path).to eq("/shelters/#{shelter1.id}")
+  end
 end
