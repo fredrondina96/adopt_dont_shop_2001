26a3ce8957842c2559c23a586b70d0ff03dff20b
Fred Rondina <fredrondina96@gmail.com>
2020-03-21 20:14:49 -0600

    Completed user story 20 plus tests

--- spec/features/shelters/user_can_view_all_shelters_spec.rb
+++ spec/features/shelters/user_can_view_all_shelters_spec.rb
@@ -26,4 +26,26 @@ RSpec.describe "shelters index page", type: :feature do
 
     expect(current_path).to eq("/shelters/#{shelter1.id}")
   end
+
+  it "there is a link to the shelter index page always at the top of the page" do
+
+    shelter1 = Shelter.create(name: 'Ricardos Reptiles', address: "1234 st", city: 'Denver', state: 'Colorado', zip: "29572")
+    shelter2 = Shelter.create(name: "Freds Felines", address: "5678 st", city: 'Seattle', state: 'Washington', zip: "390507")
+    shelter3 = Shelter.create(name: "Monroes Monitors", address: '2948 st', city: 'San Diego', state: 'California', zip: "30958")
+
+    visit "/pets"
+    expect(page).to have_link("All Shelters")
+
+    visit "/shelters"
+    expect(page).to have_link("All Shelters")
+
+    visit "/shelters/#{shelter1.id}"
+    expect(page).to have_link("All Shelters")
+
+    visit "/shelters/#{shelter2.id}"
+    expect(page).to have_link("All Shelters")
+
+    visit "/shelters/#{shelter3.id}"
+    expect(page).to have_link("All Shelters")
+  end
 end
