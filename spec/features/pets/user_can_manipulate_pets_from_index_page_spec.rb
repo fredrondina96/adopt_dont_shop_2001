fb786ace0ced59d3f5699ebf6de272dc7a50954c
Fred Rondina <fredrondina96@gmail.com>
2020-03-21 18:39:42 -0600

    Completed user story 16 plus tests

--- spec/features/pets/user_can_manipulate_pets_from_index_page_spec.rb
+++ spec/features/pets/user_can_manipulate_pets_from_index_page_spec.rb
@@ -29,4 +29,23 @@ RSpec.describe "From the pets index page", type: :feature do
     expect(page).to have_content("Yoel")
     expect(page).to have_content("A bit Bitey")
   end
+  it "user can delete pets" do
+
+    shelter1 = Shelter.create!(name: "Larry's Lizards", address: "1331 17th Street", city: 'Denver', state: 'CO', zip: "80202")
+    pet1 = shelter1.pets.create!(name: "Sam", age: "12", sex: "Female", image: "https://66.media.tumblr.com/6a9b0ea4859319c0defd9681b3a78e8f/tumblr_n8o33kXRnG1qhaglio1_r1_1280.png")
+
+    visit "/pets"
+
+    expect(page).to have_content(pet1.name)
+    expect(page).to have_content(pet1.age)
+    expect(page).to have_content(pet1.sex)
+
+    click_link "Delete Pet"
+
+    expect(current_path).to eq("/pets")
+    expect(page).to_not have_content(pet1.name)
+    expect(page).to_not have_content(pet1.age)
+    expect(page).to_not have_content(pet1.sex)
+
+  end
 end
